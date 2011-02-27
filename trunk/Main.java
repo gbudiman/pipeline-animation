e2b9cf89ae3729ee229f4580f61085efd419ad85
wahyu.g <wahyu.g@a94893ba-4788-ff59-c732-a33c63cbd166>
2011-02-27 22:03:53 +0000

    Control logic unit completed

--- trunk/Main.java
+++ trunk/Main.java
@@ -13,6 +13,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Instantiate cells
         memory instructionsMemory = new memory("Instructions Memory");
         instructionsMemory.portWrite(0, 0x3401D269);
         instructionsMemory.portWrite(4, 0x340237F1);
@@ -22,24 +23,52 @@ public class Main {
         instructionsMemory.portWrite(20, 0x00222024);
         instructionsMemory.portWrite(24, 0x3025000F);
         instructionsMemory.portWrite(28, 0x00223021);
-        instructionsMemory.portWrite(32, 0xFFFFFFFF);
+        instructionsMemory.portWrite(32, 0x0800000B);
+        instructionsMemory.portWrite(36, 0x00000000);
+        instructionsMemory.portWrite(40, 0x00000000);
+        instructionsMemory.portWrite(44, 0xFFFFFFFF);
 
         System.out.println(instructionsMemory.dump());
 
         programCounter pc = new programCounter();
-        int i = 0;
+        jumpCalculator jac = new jumpCalculator();
+        pipeline_ifid ifid = new pipeline_ifid();
+        control clu = new control();
+
+        // Instantiate nets
+        int cycle = 0;
         int netPC = 0;
+        int netAddPC = 0;
+        int netInstructions = 0;
+        boolean netJump = false;
+        int netJumpAddress = 0;
+        int netIFIDInstructions = 0;
+        int netIFIDAddPC = 0;
+
         while(instructionsMemory.portRead(pc.portPC()) != 0xffffffff) {
             try {
-                netPC = instructionsMemory.portRead(pc.portPC());
+                netPC = pc.portPC();
+                netAddPC = pc.portAddPC();
+                netInstructions = instructionsMemory.portRead(netPC);
             }
             catch (Exception e) {
                 break;
             }
             finally {
-                System.out.println(Integer.toHexString(netPC).toUpperCase());
+                System.out.print("Cycle " + cycle++ + ": " + Integer.toHexString(netInstructions).toUpperCase());
             }
-            pc.add(4);
+
+            jac.portInstructions(netInstructions, netAddPC);
+            netJump = jac.portJump();
+            netJumpAddress = jac.portJumpAddress();
+            ifid.portInput(netInstructions, netAddPC);
+            netIFIDInstructions = ifid.portInstructions();
+            netIFIDAddPC = ifid.portAddPC();
+            clu.portInput(netIFIDInstructions, false);
+            System.out.println(" | " + Integer.toHexString(netIFIDInstructions) + " --> " + clu.portState());
+
+            pc.pcUpdate(false, 0, netJump, netJumpAddress, false, false);
+            ifid.update();
         }
         System.out.println("Halt encountered. Execution successful");
     }
